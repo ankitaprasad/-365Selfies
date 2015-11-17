0d7668a947c86353f96f6810d4df11327527e285
ankitaprasad <prasad.ankita@gmail.com>
2015-11-16 19:29:24 -0800

    Initial Commit

---
+++ #365Selfies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  #365Selfies
+//
+//  Created by Ankita Prasad on 11/16/15.
+//  Copyright © 2015 Ankita Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
